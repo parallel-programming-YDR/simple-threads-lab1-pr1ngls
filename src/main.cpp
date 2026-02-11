27fc4664e789c8756539decbeda40aab4c91cbd6
MetaPhys <leonov.ve@phystech.edu>
2026-02-12 01:57:30 +0300

    init commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
-#include "Hello.h"
 #include <iostream>
 
 int main(int argc, char **argv)
 {
-  Hello hello("wanderer");
-  hello.sendMessage();
+
+  if (argc == 0 || argc > 2)
+  {
+    std::cerr << "Incorrect amount of argument\n";
+    return 2;
+  }
+  errno            = 0;
+  const long tries = std::strtol(argv[1], nullptr, 10);
+
+  if (errno == ERANGE)
+  {
+    std::cerr << "Range error\n";
+    return 2;
+  }
+  return 0;
 }
